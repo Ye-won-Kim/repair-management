ed1f0ef3c515fcc8eb28924375873e8926720108
kywon <kywon12200@gmail.com>
2026-06-26 13:19:39 +0900

    config.js 추가, config.example.js 제거

---
+++ config.js
@@ -0,0 +1,2 @@
+const API = 'https://script.google.com/macros/s/AKfycbwk-ZeN_7wKLqnireBxtEgcsEpEhpPjnPF6x0MrKbVrhrvDZfKxbFdUZDYodoJhsC8m/exec';
+const PW_HASH = '929260ad9b9ea9fe0f3553dd964f4ff3deb5792efd031a2b90f573fe91f012bb';
